f56c3ff760681927188dfda71df8f8fe1fa4c9ab
Anthony Owyeong <littledude96@gmail.com>
2019-03-02 17:16:30 -0800

    edited ignore and story board

---
+++ snap/snap/LoginViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func login(_ sender: Any) {
+        
+        
+        
+        
+        
+    }
+}
